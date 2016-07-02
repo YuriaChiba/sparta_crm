ccd62a7520b1a7b239731f9552e2b4b109bda0de
Yuria <g041m079@s.iwate-pu.ac.jp>
2016-07-02 17:13:42 +0900

    日本語化できた

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,7 +1,10 @@
 class CustomersController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_customer, only: [:show, :edit, :update, :destroy]
   def index
-    @customers = Customer.page(params[:page])
+    # @customers = Customer.page(params[:page])
+    @q = Customer.search(params[:q])
+    @customers = @q.result.page(params[:page])
   end
 
   def new
