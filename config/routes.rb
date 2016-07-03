83f38cdbb8b851d6e05f7fe966f1be37ac795aca
Yuria <g041m079@s.iwate-pu.ac.jp>
2016-07-03 09:54:16 +0900

    コメントつけた

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :customers
+
   root 'customers#index'
+  # resources :comments, only: [:create]
+  resources :comments, only: [:create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
